d24905bf582d592c22112077f93adf2facf33858
Debani <debani.cossio1217@gmail.com>
2024-11-03 01:44:09 -0600

    finished SalesContract class and added resources to readme.md

---
+++ readme.md
@@ -0,0 +1,4 @@
+## Resources
+The death of me was this annuity formula, here are the websites that helped me the most:
+- [Annuity formula](https://www.annuity.org/selling-payments/present-value/#:~:text=The%20present%20value%20of%20an%20annuity%20is%20based%20on%20a,to%20the%20Harvard%20Business%20School.)
+- [Microsoft PMT function](https://support.microsoft.com/en-us/office/pmt-function-f30c80b4-7710-4959-b10b-498c3a5a8a55)
--- src/main/java/com/ps/SalesContract.java
+++ src/main/java/com/ps/SalesContract.java
@@ -1,4 +1,62 @@
 package com.ps;
 
-public class SalesContract {
+public class SalesContract extends Contract {
+    private static final double SALES_TAX_AMOUNT = 0.05;
+    private static final double RECORDING_FEE = 100.00;
+    private boolean isFinanced;
+
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, boolean isFinanced) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.isFinanced = isFinanced;
+    }
+
+    public boolean isFinanced() {
+        return isFinanced;
+    }
+
+    public void setFinanced(boolean financed) {
+        isFinanced = financed;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        double originalPrice = getVehicleSold().getPrice();
+        double salesTax = originalPrice * SALES_TAX_AMOUNT;
+        double processingFee;
+
+        if (originalPrice < 10000) {
+            processingFee = 295.00;
+        } else {
+            processingFee = 495.00;
+        }
+        double totalPrice = originalPrice + salesTax + RECORDING_FEE + processingFee;
+        return totalPrice;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+
+        if (!isFinanced) {
+            return 0.0;
+        }
+        double totalPrice = getTotalPrice();
+        double annualInterestRate;
+        int loanTerm;
+
+        if (totalPrice >= 10000) {
+            annualInterestRate = 0.045;
+            loanTerm = 48;
+        } else {
+            annualInterestRate = 0.0525;
+            loanTerm = 24;
+        }
+        double monthlyInterestRate = annualInterestRate / 12;
+        return (totalPrice * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, - loanTerm));
+
+
+    }
+
+
+
+
 }
