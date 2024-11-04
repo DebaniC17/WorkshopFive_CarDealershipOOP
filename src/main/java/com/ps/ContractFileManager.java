7aa1821629cf3ec0b9506c3d049cad42d2bde3c8
Debani <debani.cossio1217@gmail.com>
2024-11-03 18:12:36 -0600

    added bufferwriter for sale contact

--- src/main/java/com/ps/ContractFileManager.java
+++ src/main/java/com/ps/ContractFileManager.java
@@ -1,4 +1,40 @@
 package com.ps;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class ContractFileManager {
+    public void saveContract (Contract contract) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("contract.csv", true))) {
+            if (contract instanceof SalesContract) {
+                SalesContract sale = (SalesContract) contract;
+
+                String salesContractToAdd = String.format(
+                        "SALE|%s|%s|%s|%d|%d|%s|%s|%s|%s|%d|%.2f|%.2f|%.2f|%s|%.2f|%s|%.2f",
+                        sale.getDate(),
+                        sale.getCustomerName(),
+                        sale.getCustomerEmail(),
+                        sale.getVehicleSold().getVin(),
+                        sale.getVehicleSold().getYear(),
+                        sale.getVehicleSold().getMake(),
+                        sale.getVehicleSold().getModel(),
+                        sale.getVehicleSold().getVehicleType(),
+                        sale.getVehicleSold().getColor(),
+                        sale.getVehicleSold().getOdometer(),
+                        sale.getVehicleSold().getPrice(),
+                        //sale tax amount
+                        sale.getVehicleSold().getPrice() * 0.05,
+                        //recording fee
+                        100.00,
+                        // processing fee...
+                        "processing fee",
+                        sale.getTotalPrice(),
+                        sale.isFinanced(),
+                        sale.getMonthlyPayment()
+
+                );
+                bufferedWriter.write(salesContractToAdd);
+            } else if (contract instanceof LeaseContract)
+        }
+    }
 }
